17ffee6c704519b2a03bd615feee0951b578617d
Denis Hoti <denishoti18@gmail.com>
2024-11-09 16:53:51 +0100

    Create new React App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
-// src/App.js
-import React from 'react';
-import MapComponent from './maps.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-
-      <h1>Sights to see</h1>
-
-      <MapComponent />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
