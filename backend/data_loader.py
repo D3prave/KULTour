359f02ef0691b047c832340f05d398cbe4d8cbb3
Lukas Lang <Lukas.lang@stud.ku.de>
2024-11-10 07:32:17 +0100

    Commenting, deleting old stuff

--- backend/data_loader.py
+++ backend/data_loader.py
@@ -6,8 +6,6 @@ class DataLoader:
     def __init__(self, file_path, encoding='utf-8', line_format='user item rating', sep=','):
         self.data = pd.read_csv(file_path, encoding=encoding)
         reader = Reader(line_format=line_format, sep=sep)
-        # Ensure only the necessary columns are passed to Dataset.load_from_df
-        #self.surprise_data = Dataset.load_from_df(self.data[['pageTitle (actionDetails 0)','visitorType', 'visitCount', 'continent', 'country']], reader)
         self.df = self.data.drop('eventName (actionDetails 1)', axis=1) 
         self.target = self.data['eventName (actionDetails 1)']
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.df, self.target, test_size=0.2)
@@ -22,19 +20,13 @@ class DataLoader:
     def get_features_and_target(self):
     
         return self.df, self.target
-    
-    #def get_full_dataset(self):
-    #
-    #    return self.surprise_data
 
-
-#example 
+#example (not needed)
 if __name__ == '__main__':
     loader = DataLoader('BACKEND/prepared_data.csv')
     x_train, x_test, y_train, y_test = loader.get_train_test_data()
     data_frame = loader.get_dataframe()
     features, target = loader.get_features_and_target()
-    #full_dataset = loader.get_full_dataset()
 
     print(x_train.head())
     print(data_frame.head())
